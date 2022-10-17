ca1683a17c2a412e82c7cd2e809488bf4fc023e4
jagadeeshi2i <jagadeeshj@ideas2it.com>
2022-10-17 23:35:54 +0530

    dali pipeline serialized to a file and deserialized in vision_handler.py

--- ts/torch_handler/vision_handler.py
+++ ts/torch_handler/vision_handler.py
@@ -16,7 +16,7 @@ from torchvision import transforms
 from PIL import Image
 from nvidia import dali
 from nvidia.dali import types
-from nvidia.dali.pipeline import pipeline_def
+from nvidia.dali.pipeline import pipeline_def, Pipeline
 from nvidia.dali.plugin.pytorch import DALIGenericIterator as PyTorchIterator
 
 from nvidia.dali.plugin.pytorch import LastBatchPolicy
@@ -36,28 +36,9 @@ class VisionHandler(BaseHandler, ABC):
         self.properties = context.system_properties
         if not self.properties.get("limit_max_image_pixels"):
             Image.MAX_IMAGE_PIXELS = None
-        # if "DALI_PREPROCESSING" in os.environ and os.environ["DALI_PREPROCESSING"].lower() == "true":
-        #     self.batch_tensor = []
-
-    # @pipeline_def(batch_size=5, num_threads=1, device_id=0)
-    # def dali_pipeline(self):
-    #     jpegs = dali.fn.external_source(source=[self.batch_tensor], dtype=types.UINT8)
-    #     jpegs = dali.fn.decoders.image(jpegs, device='mixed')
-    #     resized = dali.fn.resize(jpegs, size=[256])
-    #     normalized = dali.fn.crop_mirror_normalize(
-    #         resized,
-    #         crop_pos_x=0.5,
-    #         crop_pos_y=0.5,
-    #         crop=(224,224),
-    #         mean=[0.485*255, 0.456*255, 0.406*255],
-    #         std=[0.229*255, 0.224*255, 0.225*255])
-    #     return normalized
 
     def dali_preprocess(self, data):
         batch_tensor = []
-        # input_byte_arrays = [list(i['body'].values()) if 'body' in i else i['data'] for i in data]
-        # for input_array in input_byte_arrays[0]:
-        #     byte_array = bytearray(base64.b64decode(input_array))
 
         input_byte_arrays = [i['body'] if 'body' in i else i['data'] for i in data]
         for byte_array in input_byte_arrays:
@@ -66,18 +47,22 @@ class VisionHandler(BaseHandler, ABC):
 
         model_dir = self.properties.get("model_dir")
         filename = model_dir + "/model.dali"
-        pipe = Pipeline.deserialize(filename=filename, batch_size=1, num_threads=2, device_id = 0, seed = 12, prefetch_queue_depth = 1)
+        prefetch_queue_depth = 2 
+        pipe = Pipeline.deserialize(filename=filename, batch_size=5, num_threads=2, device_id = 0, seed = 12)
         pipe._max_batch_size = 1
         pipe._num_threads = 2
         pipe._device_id = 0
-        pipe.feed_input("my_source", batch_tensor)
+        for _ in range(prefetch_queue_depth):
+            pipe.feed_input("my_source", batch_tensor)
 
         datam = PyTorchIterator([pipe], ['data'], last_batch_policy=LastBatchPolicy.PARTIAL, last_batch_padded=True)
-        result = datam.next()
-        batch_tensor = []
-        # self.batch_tensor = []
-
-        # return torch.tensor(result).unsqueeze(0)
+        #result = datam.next()
+        result = []
+        for i, data in enumerate(datam):
+            result.append(data[0]['data'])
+            break
+        
+        #return torch.tensor(result).unsqueeze(0)
         return result[0].to(self.device)
 
     def preprocess(self, data):
@@ -118,4 +103,3 @@ class VisionHandler(BaseHandler, ABC):
     def get_insights(self, tensor_data, _, target=0):
         print("input shape", tensor_data.shape)
         return self.ig.attribute(tensor_data, target=target, n_steps=15).tolist()
-
