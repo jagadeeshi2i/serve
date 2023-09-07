7f257353efd8869099d8d1eb1d0970454cde2aac
jagadeesh <jagadeeshj@ideas2it.com>
2023-09-07 16:12:34 +0530

    test-1

--- kubernetes/kserve/docker_nightly.py
+++ kubernetes/kserve/docker_nightly.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 # To help discover local modules
-REPO_ROOT = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..")
+REPO_ROOT = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../..")
 sys.path.append(REPO_ROOT)
 
 from setup import get_nightly_version
